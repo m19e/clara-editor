bda17aa2e87b7d88339c41c9274ab4a82cd4ea90
m19e <nexus.me7290@gmail.com>
2022-08-24 21:15:05 +0900

    Set lexical plaintext-editor

--- renderer/components/organisms/Editor.tsx
+++ renderer/components/organisms/Editor.tsx
@@ -1,6 +1,8 @@
 import type { ComponentProps, FC } from "react"
 
 import { LexicalComposer } from "@lexical/react/LexicalComposer"
+import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin"
+import { ContentEditable } from "@lexical/react/LexicalContentEditable"
 
 const initialConfig: ComponentProps<typeof LexicalComposer>["initialConfig"] = {
   namespace: "ClaraEditor",
@@ -8,5 +10,18 @@ const initialConfig: ComponentProps<typeof LexicalComposer>["initialConfig"] = {
 }
 
 export const Editor: FC = () => {
-  return null
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <div className="vertical-editor">
+        <PlainTextPlugin
+          contentEditable={<ContentEditable className="content-editable" />}
+          placeholder={<Placeholder />}
+        />
+      </div>
+    </LexicalComposer>
+  )
+}
+
+const Placeholder: FC = () => {
+  return <div className="placeholder">いまどうしてる？</div>
 }
