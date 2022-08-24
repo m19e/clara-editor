01a8c264a31ecc8250d9c5c0e779cfc66b6aab60
m19e <nexus.me7290@gmail.com>
2022-08-24 20:37:25 +0900

    Add initial-config for lexical-editor

--- renderer/components/organisms/Editor.tsx
+++ renderer/components/organisms/Editor.tsx
@@ -1,3 +1,12 @@
-export const Editor = () => {
+import type { ComponentProps, FC } from "react"
+
+import { LexicalComposer } from "@lexical/react/LexicalComposer"
+
+const initialConfig: ComponentProps<typeof LexicalComposer>["initialConfig"] = {
+  namespace: "ClaraEditor",
+  onError: (error) => console.error(error),
+}
+
+export const Editor: FC = () => {
   return null
 }
