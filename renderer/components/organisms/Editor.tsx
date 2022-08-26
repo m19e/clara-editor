2497e73b0f647992cd2a64414a40474fabba3456
m19e <nexus.me7290@gmail.com>
2022-08-26 20:33:52 +0900

    Centering editor input area

--- renderer/components/organisms/Editor.tsx
+++ renderer/components/organisms/Editor.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from "react"
 import type { ComponentProps, FC } from "react"
 
 import { Drawer } from "react-daisyui"
@@ -16,33 +17,67 @@ const initialConfig: ComponentProps<typeof LexicalComposer>["initialConfig"] = {
 }
 
 export const Editor: FC = () => {
+  const containerRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    if (containerRef.current) {
+      // containerRef.current.setAttribute(
+      //   "style",
+      //   `
+      //   height: calc(${38}em + 7rem);
+      //   line-height: ${2};
+      //   font-size: ${1}rem;
+      //   `
+      // )
+    }
+  }, [])
+
   return (
-    <Drawer
-      side={<div className="prose bg-base-300 h-full w-1/4">drawer open!</div>}
-      open={false}
-    >
-      <div className="h-full w-full">
-        <LexicalComposer initialConfig={initialConfig}>
-          <div className="vertical relative min-h-[240px] p-6">
-            <PlainTextPlugin
-              contentEditable={
-                <ContentEditable className="h-full min-w-full font-serif focus:outline-none" />
-              }
-              placeholder={<Placeholder />}
-            />
+    <LexicalComposer initialConfig={initialConfig}>
+      <Drawer
+        side={
+          <div className="prose bg-base-100 text-base-content flex h-full w-80 flex-col gap-4 overflow-y-auto p-4">
+            drawer open!
           </div>
-          <AutoFocusPlugin defaultSelection="rootEnd" />
-          <HistoryPlugin />
-          <VerticalPlugin />
-        </LexicalComposer>
-      </div>
-    </Drawer>
+        }
+        open={false}
+      >
+        <div className="h-full w-full">
+          <div
+            id="container"
+            className="flex h-full w-full flex-col items-center justify-center"
+          >
+            <div className="w-3/4">
+              <div
+                className="scrollbar relative overflow-x-auto overflow-y-hidden py-14"
+                ref={containerRef}
+                style={{
+                  height: "calc(20em + 7em)",
+                  lineHeight: "1.5",
+                  fontSize: "1.25rem",
+                }}
+              >
+                <PlainTextPlugin
+                  contentEditable={
+                    <ContentEditable className="vertical h-full min-w-full font-serif focus:outline-none" />
+                  }
+                  placeholder={<Placeholder />}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Drawer>
+      <AutoFocusPlugin defaultSelection="rootEnd" />
+      <HistoryPlugin />
+      <VerticalPlugin />
+    </LexicalComposer>
   )
 }
 
 const Placeholder: FC = () => {
   return (
-    <div className="pointer-events-none absolute top-6 left-6 select-none text-gray-500">
+    <div className="vertical pointer-events-none absolute top-14 right-0 select-none  font-serif text-gray-500">
       執筆を始める
     </div>
   )
