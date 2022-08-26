95d54ac00517c764232ec14611a8ed5886af4b95
m19e <nexus.me7290@gmail.com>
2022-08-26 16:33:15 +0900

    Screen size root div

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -9,22 +9,9 @@ function Home() {
       <Head>
         <title>Home - Nextron (with-typescript-tailwindcss)</title>
       </Head>
-      <Editor />
-      {/* <div className="grid-col-1 grid w-full text-center text-2xl">
-        <img className="mx-auto" src="/images/logo.png" />
-        <span>⚡ Electron ⚡</span>
-        <span>+</span>
-        <span>Next.js</span>
-        <span>+</span>
-        <span>tailwindcss</span>
-        <span>=</span>
-        <span>💕 </span>
+      <div className="h-screen w-screen">
+        <Editor />
       </div>
-      <div className="mt-1 flex w-full flex-wrap justify-center">
-        <Link href="/next">
-          <a className="btn-blue">Go to next page</a>
-        </Link>
-      </div> */}
     </React.Fragment>
   )
 }
