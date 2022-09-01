e05d7339be4d283a89ed0d9458e786ec9361589e
m19e <nexus.me7290@gmail.com>
2022-09-01 21:52:37 +0900

    Add chevron for inc/dec button

--- renderer/components/organisms/Footer.tsx
+++ renderer/components/organisms/Footer.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable tailwindcss/enforces-negative-arbitrary-values */
+
 export const Footer = () => {
   return (
     <div className="fixed bottom-0 w-full">
@@ -9,24 +10,75 @@ export const Footer = () => {
   )
 }
 
-// Control container height => 24rem
-// Control label height => 3rem
 const Control = () => {
+  const handleClickDemo = () => {
+    // empty
+  }
+
   return (
-    <div className="-bottom-[8rem absolute -bottom-0 flex w-full justify-center delay-500 duration-150 hover:!bottom-0 group-hover:-bottom-[calc(8rem-3rem)]">
-      <div className="bg-base-100 flex h-[8rem] w-full flex-col items-center shadow">
-        <div className="bg-gray-30 flex h-[3rem] items-center">
-          <span className="text-base">書式設定</span>
+    <div
+      className={`-bottom-[6.5rem absolute -bottom-0 flex w-full justify-center delay-500 duration-150 hover:!bottom-0 group-hover:-bottom-[calc(6.5rem-1rem)]`}
+    >
+      <div
+        className={`bg-base-100 flex h-[6.5rem] w-full flex-col items-center shadow`}
+      >
+        <div className="flex h-[1rem] w-1/12 items-center justify-center">
+          <span className="h-1 w-full rounded bg-gray-400"></span>
         </div>
-        <div className="flex-1">
-          <div className="flex h-[3rem] items-center gap-4">
-            <span>設定1</span>
-            <span>設定2</span>
-            <span>設定3</span>
-            <span>設定4</span>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="flex items-center gap-4">
+            <div className="nested-group flex flex-col items-center">
+              <Chevron type="inc" onClick={handleClickDemo} disabled={false} />
+              <span>大きさ {"{fs}"}</span>
+              <Chevron type="dec" onClick={handleClickDemo} disabled={false} />
+            </div>
+            <div className="nested-group flex flex-col items-center">
+              <Chevron type="inc" onClick={handleClickDemo} disabled={true} />
+              <span>大きさ {"{fs}"}</span>
+              <Chevron type="dec" onClick={handleClickDemo} disabled={true} />
+            </div>
+            <div className="nested-group flex flex-col items-center">
+              <Chevron type="inc" onClick={handleClickDemo} disabled={false} />
+              <span>大きさ fs</span>
+              <Chevron type="dec" onClick={handleClickDemo} disabled={false} />
+            </div>
           </div>
         </div>
       </div>
     </div>
+  )
+}
+
+type Props = {
+  type: "inc" | "dec"
+  onClick: () => void
+  disabled: boolean
+}
+
+const Chevron = ({ type, onClick, disabled }: Props) => {
+  const d =
+    type === "inc"
+      ? "M4.5 15.75l7.5-7.5 7.5 7.5"
+      : "M19.5 8.25l-7.5 7.5-7.5-7.5"
+
+  return (
+    <button
+      className="btn btn-xs btn-ghost no-animation w-full !bg-opacity-0 text-gray-400 transition-none hover:text-gray-600 active:text-gray-800"
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <span className="ng-hover:opacity-100 opacity-0 transition-opacity">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1}
+          stroke="currentColor"
+          className="h-5 w-5"
+        >
+          <path strokeLinecap="round" strokeLinejoin="round" d={d} />
+        </svg>
+      </span>
+    </button>
   )
 }
