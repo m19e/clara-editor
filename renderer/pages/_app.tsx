66e53ada4e372650bfbd12b8f2fbc3cd3d689dc9
m19e <nexus.me7290@gmail.com>
2022-09-06 23:28:50 +0900

    Set theme-provider in _app

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,14 +1,17 @@
 import React from "react"
 import type { AppProps } from "next/app"
 import { RecoilRoot } from "recoil"
+import { ThemeProvider } from "next-themes"
 
 import "@/styles/globals.css"
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <ThemeProvider defaultTheme="light">
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </ThemeProvider>
   )
 }
 
