ba858b4a0358a169a62dfb10e385c29f6f79106f
m19e <nexus.me7290@gmail.com>
2022-09-14 20:21:55 +0900

    Display fallback title

--- renderer/foundations/MetaHead.tsx
+++ renderer/foundations/MetaHead.tsx
@@ -1,14 +1,23 @@
 import Head from "next/head"
 import { parse } from "path"
 
-import { useIsSaved, useDraftPath } from "@/hooks"
+import { useIsFallback, useIsSaved, useDraftPath } from "@/hooks"
 
 export const MetaHead = () => {
+  const [isFallback] = useIsFallback()
   const [isSaved] = useIsSaved()
   const [draftPath] = useDraftPath()
 
   const mark = isSaved ? "" : "*"
   const title = draftPath ? parse(draftPath).base : "無題"
+
+  if (isFallback) {
+    return (
+      <Head>
+        <title>読み込み中 - Clara Editor</title>
+      </Head>
+    )
+  }
 
   return (
     <Head>
