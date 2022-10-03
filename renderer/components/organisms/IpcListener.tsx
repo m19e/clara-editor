9f0924ba9be9a339b2c88095810e865e0a232816
m19e <nexus.me7290@gmail.com>
2022-10-03 20:51:47 +0900

    Select all when recieve from context-menu

--- renderer/components/organisms/IpcListener.tsx
+++ renderer/components/organisms/IpcListener.tsx
@@ -1,6 +1,9 @@
 import { ipcRenderer } from "electron"
 import { useEffect } from "react"
 import { useTheme } from "next-themes"
+import { $getSelection, $isRangeSelection } from "lexical"
+import { $selectAll } from "@lexical/selection"
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext"
 
 import { useDisplayCharCount, useDraftPath } from "@/hooks"
 
@@ -9,23 +12,40 @@ export const IpcListener = () => {
   const [, setDraftPath] = useDraftPath()
   const [, setDisplayCharCount] = useDisplayCharCount()
 
+  const [editor] = useLexicalComposerContext()
+
+  useEffect(() => {
+    ipcRenderer.on("toggle-color-theme", () => {
+      setTheme(theme === "dark" ? "light" : "dark")
+    })
+
+    return () => {
+      ipcRenderer.removeAllListeners("toggle-color-theme")
+    }
+  }, [theme])
+
   useEffect(() => {
     ipcRenderer.on("recieve-draft-path", (_, payload: string) => {
       setDraftPath(payload)
     })
-    ipcRenderer.on("toggle-color-theme", () => {
-      setTheme(theme === "dark" ? "light" : "dark")
-    })
     ipcRenderer.on("toggle-char-count", () => {
       setDisplayCharCount((prev) => !prev)
+    })
+    ipcRenderer.on("select-all", () => {
+      editor.update(() => {
+        const selection = $getSelection()
+        if ($isRangeSelection(selection)) {
+          $selectAll(selection)
+        }
+      })
     })
 
     return () => {
       ipcRenderer.removeAllListeners("recieve-draft-path")
-      ipcRenderer.removeAllListeners("toggle-color-theme")
       ipcRenderer.removeAllListeners("toggle-char-count")
+      ipcRenderer.removeAllListeners("select-all")
     }
-  }, [theme])
+  }, [])
 
   return null
 }
