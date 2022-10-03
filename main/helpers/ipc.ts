f897c7e0bbfa2b4918a6ec76e6938171d949def7
m19e <nexus.me7290@gmail.com>
2022-10-03 21:22:01 +0900

    Add registerer for ipc-main

--- main/helpers/ipc.ts
+++ main/helpers/ipc.ts
@@ -1,29 +1,48 @@
 import { BrowserWindow, ipcMain, dialog } from "electron"
-import type { OpenDialogOptions, SaveDialogOptions } from "electron"
+import type {
+  IpcMainEvent,
+  OpenDialogOptions,
+  SaveDialogOptions,
+} from "electron"
+
+type RendererChannel = "open-file-dialog" | "open-save-dialog"
+
+const registerIpcListener = (
+  channel: RendererChannel,
+  listener: (event: IpcMainEvent, ...args: any[]) => void
+): void => {
+  ipcMain.on(channel, listener)
+}
 
 export const addIpcListener = (mainWindow: BrowserWindow) => {
-  ipcMain.on("open-file-dialog", async (event, payload?: OpenDialogOptions) => {
-    if (!mainWindow) {
-      event.reply("open-file-dialog")
-      return
-    }
+  registerIpcListener(
+    "open-file-dialog",
+    async (event, payload?: OpenDialogOptions) => {
+      if (!mainWindow) {
+        event.reply("open-file-dialog")
+        return
+      }
 
-    const value = await dialog.showOpenDialog(mainWindow, {
-      ...payload,
-    })
-    event.reply("open-file-dialog", value)
-  })
-  ipcMain.on("open-save-dialog", async (event, payload?: SaveDialogOptions) => {
-    if (!mainWindow) {
-      event.reply("open-save-dialog")
-      return
+      const value = await dialog.showOpenDialog(mainWindow, {
+        ...payload,
+      })
+      event.reply("open-file-dialog", value)
     }
+  )
+  registerIpcListener(
+    "open-save-dialog",
+    async (event, payload?: SaveDialogOptions) => {
+      if (!mainWindow) {
+        event.reply("open-save-dialog")
+        return
+      }
 
-    const value = await dialog.showSaveDialog(mainWindow, {
-      ...payload,
-    })
-    event.reply("open-save-dialog", value)
-  })
+      const value = await dialog.showSaveDialog(mainWindow, {
+        ...payload,
+      })
+      event.reply("open-save-dialog", value)
+    }
+  )
 }
 
 type MainChannel =
