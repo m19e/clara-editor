d8e20b4e680f1a5d76c0b37b4034ed8134c27859
m19e <nexus.me7290@gmail.com>
2022-10-14 16:23:08 +0900

    Fix line-number crash

--- renderer/components/organisms/Editor.tsx
+++ renderer/components/organisms/Editor.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState, useEffect, useMemo } from "react"
+import { useRef, useState, useEffect, useCallback, useMemo } from "react"
 import type { ComponentProps, FC, WheelEvent } from "react"
 import { $getRoot, $getSelection, $isRangeSelection } from "lexical"
 import type { EditorState } from "lexical"
@@ -59,11 +59,14 @@ export const Editor: FC = () => {
 
   const [lineCount, setLineCount] = useState(1)
 
-  const updateLineNumCount = (editorWidth: number) => {
-    const lineWidth = 16 * fs * lh
-    const count = Math.round(editorWidth / lineWidth)
-    setLineCount(count || 1)
-  }
+  const updateLineNumCount = useCallback(
+    (editorWidth: number) => {
+      const lineWidth = 16 * fs * lh
+      const count = Math.round(editorWidth / lineWidth)
+      setLineCount(count + 2)
+    },
+    [fs, lh]
+  )
 
   useEffect(() => {
     if (editorRef.current) {
@@ -89,7 +92,7 @@ export const Editor: FC = () => {
     return () => {
       resizeObs.disconnect()
     }
-  }, [editorRef, displayLN])
+  }, [editorRef, displayLN, updateLineNumCount])
 
   const handleWheel = (e: WheelEvent<HTMLElement>) => {
     if (scrollRef.current) {
@@ -167,8 +170,10 @@ export const Editor: FC = () => {
 }
 
 const LineNumber: FC<{ count: number }> = ({ count }) => {
+  const [fs] = useFontSize()
+  const [lh] = useLineHeight()
   const [display] = useDisplayLineNumber()
-  const opacity = display ? "opacity-50" : "opacity-0"
+
   const labels = useMemo(
     () =>
       Array.from({ length: count }).map((_, i) => {
@@ -178,16 +183,28 @@ const LineNumber: FC<{ count: number }> = ({ count }) => {
       }),
     [count]
   )
+  const opacity = display ? "opacity-50" : "opacity-0"
 
   return (
     <div
-      className={`flex w-full select-none flex-row-reverse transition-opacity ${opacity}`}
+      className="relative overflow-x-hidden"
+      style={{ height: `calc(${fs}rem * ${lh})` }}
     >
-      {labels.map((label, i) => (
-        <span key={i} className="w-full text-center">
-          {label}
-        </span>
-      ))}
+      <div
+        className={`absolute top-0 right-0 flex select-none flex-row-reverse transition-opacity ${opacity}`}
+      >
+        {labels.map((label, i) => (
+          <span
+            key={i}
+            className="w-full text-center"
+            style={{
+              width: `calc(${fs}rem * ${lh})`,
+            }}
+          >
+            {label}
+          </span>
+        ))}
+      </div>
     </div>
   )
 }
